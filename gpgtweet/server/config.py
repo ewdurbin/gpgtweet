d31bee990d2e5ebe6e53895f0543aef2480eca61
Ernest W Durbin III <ernestd@ldap.ernestdurbin.com>
2012-07-20 11:14:51 -0400

    pushing configuration stuffs into their own bizness

---
+++ gpgtweet/server/config.py
@@ -0,0 +1,21 @@
+
+import os.path
+import ConfigParser
+
+class GPGTweetConfig:
+
+    def __init__(self, config_file):
+        self.storage_dir = None
+        self.cookie_secret = None
+        self.consumer_key = None
+        self.consumer_secret = None
+        if os.path.isfile(config_file):
+            config = ConfigParser.RawConfigParser()
+            config.read(config_file)
+            storage_dir = config.get('GPGTweet', 'storage_dir') 
+            storage_dir = os.path.expanduser(storage_dir)
+            self.storage_dir = os.path.abspath(storage_dir)
+            print self.storage_dir
+            self.cookie_secret = config.get('Tornado', 'cookie_secret') 
+            self.consumer_key = config.get('TwitterApi', 'consumer_key')
+            self.consumer_secret = config.get('TwitterApi', 'consumer_secret')

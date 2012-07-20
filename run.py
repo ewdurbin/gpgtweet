a9119544d917262ccb3b9fdd89154d945a798e6d
Ernest W. Durbin III <ewdurbin@gmail.com>
2012-07-20 11:30:24 -0400

    add command line parser for configuration file location

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 
 import sys
+from optparse import OptionParser
 
 import tornado.web
 import tornado.ioloop
@@ -9,9 +10,19 @@ from gpgtweet.server import auth
 from gpgtweet.server import messages 
 from gpgtweet.server.config import GPGTweetConfig
 
+def parse_options():
+    parser = OptionParser()
+    parser.add_option('-c', '--config-file',
+                      help="Configuration File for GPGTweet Server",
+                      default="gpgtweet.cfg", action="store", type="string",
+                      dest="config_file")
+    (options, args) = parser.parse_args()
+    return (options, args)
+
 if __name__ == "__main__":
-    config_file = sys.argv[1]
-    config = GPGTweetConfig(config_file)
+    (options, args) = parse_options()
+
+    config = GPGTweetConfig(options.config_file)
 
     settings = {
         'twitter_consumer_key': config.consumer_key,
